0cca712abcca6f7829d5fa9ed4a8123d77503999
siva_1316 <sivateja.reddy@aciesglobal.com>
2025-02-05 09:35:34 +0530

    Credit card insights

---
+++ Semantic_model/Model/sqls/credit_card_data.sql
@@ -0,0 +1,8 @@
+SELECT
+    Client_Num,
+    Credit_Limit,
+    Total_Trans_Amt,
+    Delinquent_Acc
+FROM 
+    lakehouse.siva.credit_data_cc;
+    
---
+++ Semantic_model/Model/sqls/customer_data.sql
@@ -0,0 +1,8 @@
+SELECT
+    Client_Num,
+    Personal_loan,
+    Customer_Job,
+    Income,
+    Cust_Satisfaction_Score
+FROM
+    lakehouse.siva.costomer_data_cc;
