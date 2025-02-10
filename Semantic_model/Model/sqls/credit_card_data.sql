b3ace43e913fa9be9f83ba0f7d859e8d4fbba0d8
siva_1316 <sivateja.reddy@aciesglobal.com>
2025-02-10 09:58:56 +0530

    Data update

--- Semantic_model/Model/sqls/credit_card_data.sql
+++ Semantic_model/Model/sqls/credit_card_data.sql
@@ -1,8 +1,27 @@
-SELECT
+SELECT 
     Client_Num,
     Credit_Limit,
     Total_Trans_Amt,
-    Delinquent_Acc
+    Avg_Utilization_Ratio,
+    Total_Revolving_Bal,
+    Delinquent_Acc,
+    
+    -- Calculate Risk Score
+    (0.4 * Avg_Utilization_Ratio + 
+     0.3 * Total_Revolving_Bal + 
+     0.5 * Delinquent_Acc - 
+     0.2 * Credit_Limit - 
+     0.1 * Total_Trans_Amt) AS Risk_Score,
+    
+    -- Categorizing customers as risky based on threshold, considering greater than 70% as risk.
+    CASE 
+        WHEN (0.4 * Avg_Utilization_Ratio + 
+              0.3 * Total_Revolving_Bal + 
+              0.5 * Delinquent_Acc - 
+              0.2 * Credit_Limit - 
+              0.1 * Total_Trans_Amt) > 0.7 THEN 'Risky'
+        ELSE 'Not Risky'
+    END AS Risk_Category
+
 FROM 
     lakehouse.siva.credit_data_cc;
-    
